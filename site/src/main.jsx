c48c4213e31ff8b945bd84d7b55681b738ac1b03
Safiyahf <safiyah.sf@gmail.com>
2024-10-27 00:16:09 -0400

    reverting?

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
 //import App from './App.jsx'
 import Home from './Home'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Home />
-=======
-import App from './App.jsx'
-import Home from './Home.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Home/>
->>>>>>> dcb2b2e867342e5dde9756ca8861f496bf959921
   </StrictMode>,
 )
